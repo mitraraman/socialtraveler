d71ca7c68adfff6b6eff8b521f403fa609c13811
Folashade Okunubi <foo@andrew.cmu.edu>
2013-05-03 13:27:39 -0400

    Added comments to the homepage (unstyled)

--- app.js
+++ app.js
@@ -2,9 +2,43 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+// The global datastore 
+var messages;
+
 app.get('/', function(request, response) {
 	response.send('Hellow World!');
 });
+
+
+// get all messages
+app.get("/messages", function(request, response){
+  response.send({
+    messages: messages,
+    success: true
+  });
+});
+
+// create new message
+app.post("/messages", function(request, response) {
+  var item = {"message": request.body.message,
+              "user": request.body.user,
+              "date": new Date()};
+
+  var successful = (item.message !== undefined);
+
+  if (successful) {
+    messages.push(item);
+    writeFile("data/data.txt", JSON.stringify(messages));
+  } else {
+    item = undefined;
+  }
+
+  response.send({ 
+    item: item,
+    success: successful
+  });
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
---
+++ js/script.js
@@ -0,0 +1,67 @@
+// The global datastore 
+var messages = [];
+
+function getParam( name ){
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");  
+  var regexS = "[\\?#]"+name+"=([^&#]*)";  
+  var regex = new RegExp( regexS );  
+  var results = regex.exec( window.location.href); 
+  if( results == null )    return "";  
+  else    
+    return results[1];
+}
+
+
+function message(){
+  var user = "none";
+
+  var message =": <br>" + $("#message-input").val();
+  // addMessage(message,user);
+  messages.push({message:message, user: user, date: new Date()});
+  $("#message-input").val("");
+
+	console.log(message);
+	console.log(messages);
+	
+
+  refreshDOM();
+}
+
+function addMessage(message, user){
+	$.ajax({
+		type:"post",
+		data: {"message": message,
+    "user": user},
+    url: "/messages",
+    success: function (data){}
+  });
+}
+
+
+// Implement refreshDOM()
+function refreshDOM(){
+  if (messages === undefined) return;
+  var container = $("#chatroom");
+  container.empty();
+  $("#message-input").empty();
+
+  for (var i = 0; i < messages.length; i++){
+    var message = messages[i].message;
+    var li = $("<li>");
+    li.addClass("bubble");
+    var user = messages[i].user;
+
+    if (user === (getParam('user'))){
+      li.addClass("myself");
+    }
+    else{
+      li.addClass("others");
+    }
+    var post = li.html(message);
+    li.append(post);
+    container.append(li);
+  }
+  if (window.location.href.indexOf("game") !== -1){
+    container.scrollTop($("#chatroom")[0].scrollHeight);
+  }
+}
